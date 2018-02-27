9bacee2d41755ab8e9ad8740ef65a43aaa221eb4
Sako Kassabian <skass@firstent.org>
2018-02-27 10:32:14 -0800

    game is read

--- js/app.js
+++ js/app.js
@@ -13,51 +13,44 @@ $(document).ready(function(){
   	});
   	//variables
   	//why do i have to put 1 instead of 0?
-  	var guessCount = 1;
-  	var secretNum = Math.floor((Math.random() * 100) + 1);
+  	var guessCount = 0;
+  	var secretNum = Math.floor((Math.random() * 1000) + 1);
   	// for the cheaters
   	console.log(secretNum);
   	$('#guessButton').click(function(e){
   		e.preventDefault();
-  		var guesses = $('#userGuess').val();
-
-  		if(guesses >= 1 && guesses <= 100){
-  			$('#count').text(guessCount++);
-  			$('#guessList').append("<li class='guessesList'>" + guesses + "</li>");
-  		}else{
-  			//enter text that they need to put an accepted
-  			alert("Please enter a valid number (1-100)");	
-  		}
-		//user needs feedback on guess
-		//feedback appears in #feedback
-		if (guesses > secretNum){
-			if((guesses - secretNum) >= 20 ){
-	  			$('#feedback').text("Wow! You're freezing");
-	  		}else if ((guesses - secretNum) <= 20 && (secretNum - guesses) !== 0){
-	  			$('#feedback').text('Hot!');
-	  		}else if ((guesses - secretNum) == 0){
-	  			$("#feedback").text("Wow. You got it!");
-	  			if(confirm("Great Job! Play again?") == true){
-	  				newGame();
-	  			}else{
-	  				$("#feedback").text("Click + New Game to start over");
-	  			}
-	  		}
+		
+  		var guess = $('#userGuess').val();
+		if(guess > 0 && guess <= 1000){
+			guessCount++;
+			
+			$('#guessList').append("<li class='guessesList'>" + guess + "</li>");
+			
+			if (guess < secretNum)
+			{
+				$('#feedback').text("Your guess is too LOW");
+			}
+			else if (guess > secretNum)
+			{
+				$('#feedback').text("Your guess is too HIGH");
+			}
+			else 
+			{
+				$('#feedback').text("Congratulations! You guessed the right number!");
+				$('#guessButton').prop("disabled",true);
+			}
+			
+			$('#guessesLeft').text(10-guessCount);
+			
+			if (guessCount > 10)
+			{
+				$('#feedback').text("The target number was " + secretNum + ". You couldn't get it. You will soon be escorted out.");
+				$('#guessButton').prop("disabled",true);
+			}
+		}else{
+			$('#feedback').text("Please enter a valid number");
 		}
-			else{
-			if((secretNum - guesses) >= 20 ){
-  				$('#feedback').text("Wow! You're freezing");
-  			}else if ((secretNum - guesses) <= 20 && (secretNum - guesses) !== 0){
-  				$('#feedback').text('Hot!');
-  			}else if ((secretNum - guesses) == 0){
-  				$("#feedback").text("Wow. You got it!");
-  				if(confirm("Great Job! Play again?") == true){
-  				newGame();
-  				}else{
-  				$("#feedback").text("Click + New Game to start over");
-  				}
-  			}}
-  		
+		$('#userGuess').val("");
   	});
 
   	$(".new").click(newGame);
@@ -65,21 +58,54 @@ $(document).ready(function(){
   	function newGame(){
 	//start a new game
 	//remove guess #
-	$("#count").text("0");
+	$("#guessesLeft").text("10");
 	//remove list of guesses
 	$("#guessList li").remove();
 	//remove hot/cold text
 	$("#feedback").text("Make your Guess!");
 	//remove guess input #
 	$("#userGuess").val("")
-  	//secret 1-100 number generated
-  	secretNum = Math.floor((Math.random() * 100) + 1);
-  	guessCount = 1;
+  	//secret 1-1000 number generated
+  	secretNum = Math.floor((Math.random() * 1000) + 1);
+  	guessCount = 0;
   	// for the cheaters
   	console.log(secretNum);
 }
 
+	$(".comp").click(function(){		
+		var arr = []
+		for(var i=1; i <= 1000; i++ ){
+			arr.push(i);
+		}
+		$("#compGuess, #aiButton").show();
+		$("#aiButton").on("click", function(e){
+			e.preventDefault();
+			var aiNum = $("#compGuess").val();
+			binarySearch(arr, secretNum);
+		});
+	});
+	var binarySearch = function(array, value) {
+    var guess,
+        min = 1,
+        max = array.length;
 
+    for(var i=1; i <= 10; i++){
+		setTimeout(function(){
+        guess = Math.floor((min + max) /2);
+		$("#userGuess").val(guess);
+				
+			if(guess < value)
+				min = guess + 1;
+			else if (guess > value)
+				max = guess - 1;
+			
+			$("#guessButton").click();
+				 }, 3000);
+		
+	 }
+	
+     return -1;
+}
 
 
 });
